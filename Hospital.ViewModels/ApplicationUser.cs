dd6d49eddbae1134aa7cb27847aab0812578eaa3
Md.Ishak Jewel <ishakjewel@gmail.com>
2024-05-22 15:55:34 +0600

    Add project files.

---
+++ Hospital.ViewModels/ApplicationUser.cs
@@ -0,0 +1,10 @@
+﻿using Microsoft.AspNet.Identity.EntityFramework;
+
+namespace Hospital.ViewModels
+{
+    public class ApplicationUser : IdentityUser
+    {
+        public string UserName { get; set; }
+        public string Email { get; set; }
+    }
+}
